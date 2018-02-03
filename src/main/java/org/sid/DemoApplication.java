9497eea598599fff010f216ba85d2ee8e6801fff
Mondher GHARBI <mgharbi@spark.local>
2018-02-03 21:24:18 +0100

    Crud Spring Boot application

---
+++ src/main/java/org/sid/DemoApplication.java
@@ -0,0 +1,31 @@
+package org.sid;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import org.sid.dao.ContactRepository;
+import org.sid.entities.Contact;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner{
+
+	@Autowired
+	private ContactRepository contactRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		DateFormat df= new SimpleDateFormat("dd/MM/yyyy");
+		contactRepository.findAll().forEach(c->{
+        	System.out.println(c.getNom());
+        	
+        });
+		
+	}
+}
